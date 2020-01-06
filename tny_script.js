db929627cc43ba30e46ecc1821c99d6097bef6f7
Aspen <asmith184@west-mec.org>
2020-01-06 15:26:41 -0700

    asdfghjkl

---
+++ tny_script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Tutorial Case
+
+   Countdown Clock
+   Author: Aspen Smith
+   Date:   1-6-19
+
+*/
+
+document.getElementById("dateNow");
+
+document.getSelection.name("Jeff");
+
