55515c0af17f1860780d01932088c813184dbda5
Aspen <asmith184@west-mec.org>
2020-01-09 14:28:11 -0700

    hjg

--- tny_script.js
+++ tny_script.js
@@ -11,7 +11,41 @@
 
 */
 
-document.getElementById("dateNow");
+// runClock();
+// setInterval("runClock()", 100);
 
-document.getSelection.name("Jeff");
 
+// display the current date and time
+
+document.getElementById("dateNow").innerHTML = "mm/dd/yyy <br/> hh:mm:ss";
+
+// display the time left until new year's eve
+
+document.getElementById("days").textContent = "DD";
+document.getElementById("hrs").textContent = "HH";
+document.getElementById("mins").textContent = "MM";
+document.getElementById("secs").textContent = "SS";
+
+var date = new Date("January 7, 2020 1:54:23");
+
+var dateStr = date.toDateString();
+var timeStr = date.toLocaleTimeString();
+
+document.getElementById("dateNow").innerHTML = dateStr + "<br/>" + timeStr;
+
+var daysLeft = 365 - date.getDate();
+var hrsLeft = 24 - date.getHours();
+var minsLeft = 
+
+document.getElementById("days").textContent = daysLeft;
+document.getElementById("hrs").textContent = hrsLeft;
+
+var height = Math.random();
+
+function solver(){
+   var area = height * length;
+
+   return ;
+}
+
+console.log(solver());
